9af540bce29912e6a13d0ef349ff65d08c8082bf
bhodgen <bhodgen@armt.com>
2013-06-06 14:02:34 -0500

    demo-7

--- site/js/app.js
+++ site/js/app.js
@@ -1,49 +1,44 @@
 var app = angular.module('MyAngularApp',[]);
 
-var FactoryFunction = function() {
-    return {
-        foo: 'A value for foo',
-        bar: 'A value for bar',
-        baz: 'A value for baz',
-        random: ''
-    };
+var model = {
+    foo: 'A different value for foo',
+    bar: 'A different value for bar',
+    baz: 'A different value for baz',
+    random: ''
 };
 
-var AnotherFactoryFunction = function() {
-    return {
-        foo: 'A different value for foo',
-        bar: 'A different value for bar',
-        baz: 'A different value for baz',
-        random: ''
-    };
+var ServiceFunction = function(TheModel) {
+    this.model = TheModel;
+    this.model.random = this.generateRandomString();
 };
 
-var SimpleControllerFunction = function($scope,someothername) {
-    $scope.model = someothername;
+ServiceFunction.prototype.generateRandomString = function() {
+    var stringArray = [];
+
+    for (var i =0; i < 10; i++) {
+        var charCode = Math.floor(Math.random()*26);
+        var char = String.fromCharCode(charCode + 97);
+        stringArray.push(char);
+    }
+
+    return stringArray.join('');
+};
+
+
+var SimpleControllerFunction = function($scope,myservice) {
+    $scope.model = myservice.model;
     $scope.newRandom = '';
 
     $scope.onClick = function() {
-        $scope.model.random = $scope.GenerateRandomNumber();
+        $scope.model.random = myservice.generateRandomString();
     };
 
-    $scope.GenerateRandomNumber = function() {
-        var stringArray = [];
-
-        for (var i =0; i < 10; i++) {
-            var charCode = Math.floor(Math.random()*26);
-            var char = String.fromCharCode(charCode + 97);
-            stringArray.push(char);
-        }
-
-        return stringArray.join('');
-    };
-
-    $scope.newRandom = $scope.GenerateRandomNumber();
+    $scope.newRandom = myservice.generateRandomString();
 };
 
-app.factory('ModelVal',FactoryFunction);
-app.factory('OtherModelVal',AnotherFactoryFunction);
+app.value('MyModel',model);
+app.service('SimpleService',['MyModel',ServiceFunction]);
 
-app.controller('SimpleControllerA',['$scope','ModelVal',SimpleControllerFunction]);
-app.controller('SimpleControllerB',['$scope','OtherModelVal',SimpleControllerFunction]);
+app.controller('SimpleControllerA',['$scope','SimpleService',SimpleControllerFunction]);
+app.controller('SimpleControllerB',['$scope','SimpleService',SimpleControllerFunction]);
 

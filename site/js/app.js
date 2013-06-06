c435f26dcaae9385af5ba3f146768449ee1d4133
bhodgen <bhodgen@armt.com>
2013-06-06 16:00:11 -0500

    demo-11

--- site/js/app.js
+++ site/js/app.js
@@ -3,10 +3,24 @@ var app = angular.module('MyAngularApp',[]);
 var DirectiveFactoryFunction = function() {
     var DirectiveDefinitionObject = {
         restrict: 'E',
-        template: '<p>This is kind of a dumb directive!!!</p>'
+        scope: {
+            localName:'@',
+            boundName:'='
+        },
+        templateUrl: './partials/dir.html'
     };
 
     return DirectiveDefinitionObject;
 };
 
+var SimpleControllerFunction = function() {
+    this.model = {
+        foo: 'A value for foo',
+        bar: 'A value for bar',
+        baz: 'A value for baz'
+    };
+
+};
+
 app.directive('dumbDirective',DirectiveFactoryFunction);
+app.controller('SimpleController',SimpleControllerFunction);

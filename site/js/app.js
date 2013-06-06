b9215b92d9d624a24f238399ff38ec42fc1b3c6e
bhodgen <bhodgen@armt.com>
2013-06-06 14:16:17 -0500

    demo-8

--- site/js/app.js
+++ site/js/app.js
@@ -1,29 +1,38 @@
 var app = angular.module('MyAngularApp',[]);
 
-var model = {
-    foo: 'A different value for foo',
-    bar: 'A different value for bar',
-    baz: 'A different value for baz',
-    random: ''
+var SimpleServiceProvider = function() {
+    var localModel = {
+        foo: 'A different value for foo',
+        bar: 'A different value for bar',
+        baz: 'A different value for baz',
+        random: ''
+    };
+
+    var ServiceFunction = function() {
+        this.model = localModel;
+        this.model.random = this.generateRandomString();
+    };
+
+    ServiceFunction.prototype.generateRandomString = function() {
+        var stringArray = [];
+
+        for (var i =0; i < 10; i++) {
+            var charCode = Math.floor(Math.random()*26);
+            var char = String.fromCharCode(charCode + 97);
+            stringArray.push(char);
+        }
+
+        return stringArray.join('');
+    };
+
+    this.$get = function() {
+        return new ServiceFunction();
+    };
+
+    this.setModel = function(newModel) {
+        localModel = newModel;
+    };
 };
-
-var ServiceFunction = function(TheModel) {
-    this.model = TheModel;
-    this.model.random = this.generateRandomString();
-};
-
-ServiceFunction.prototype.generateRandomString = function() {
-    var stringArray = [];
-
-    for (var i =0; i < 10; i++) {
-        var charCode = Math.floor(Math.random()*26);
-        var char = String.fromCharCode(charCode + 97);
-        stringArray.push(char);
-    }
-
-    return stringArray.join('');
-};
-
 
 var SimpleControllerFunction = function($scope,myservice) {
     $scope.model = myservice.model;
@@ -36,9 +45,21 @@ var SimpleControllerFunction = function($scope,myservice) {
     $scope.newRandom = myservice.generateRandomString();
 };
 
-app.value('MyModel',model);
-app.service('SimpleService',['MyModel',ServiceFunction]);
+app.provider('SimpleService',SimpleServiceProvider);
 
 app.controller('SimpleControllerA',['$scope','SimpleService',SimpleControllerFunction]);
 app.controller('SimpleControllerB',['$scope','SimpleService',SimpleControllerFunction]);
+
+var AppConfig = function(SimpleProvider) {
+    var myNewModel = {
+        foo: 'A configured value for foo',
+        bar: 'A configured value for bar',
+        baz: 'A configured value for baz',
+        random: ''
+    };
+
+    SimpleProvider.setModel(myNewModel);
+};
+
+app.config(['SimpleServiceProvider',AppConfig]);
 

1835a85dc1a549a8723507d61181d2d6b1f6db8f
bhodgen <bhodgen@armt.com>
2013-06-06 12:33:23 -0500

    demo-2

--- site/js/app.js
+++ site/js/app.js
@@ -1,9 +1,25 @@
 var app = angular.module('MyAngularApp',[]);
 
-var SimpleController = function($scope) {
+var ParentController = function($scope) {
     $scope.model = {
         foo: 'A value for foo',
         bar: 'A value for bar',
         baz: 'A value for baz'
-    }
-}
+    };
+};
+
+var ChildControllerLeft = function($scope) {
+    $scope.leftmodel = {
+        foo: 'Left Child Value for foo',
+        bar: 'Left Child Value for bar',
+        baz: 'Left Child Value for baz'
+    };
+};
+
+var ChildControllerRight = function($scope) {
+    $scope.rightmodel = {
+        foo: 'Right Child Value for foo',
+        bar: 'Right Child Value for bar',
+        baz: 'Right Child Value for baz'
+    };
+};

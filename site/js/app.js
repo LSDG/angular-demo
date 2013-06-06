d33beb8e75bf39a81a09e84c5c86f171513b61b3
bhodgen <bhodgen@armt.com>
2013-06-06 15:36:50 -0500

    demo-9

--- site/js/app.js
+++ site/js/app.js
@@ -1,65 +1,38 @@
 var app = angular.module('MyAngularApp',[]);
 
-var SimpleServiceProvider = function() {
-    var localModel = {
-        foo: 'A different value for foo',
-        bar: 'A different value for bar',
-        baz: 'A different value for baz',
-        random: ''
+
+var SimpleControllerFunction = function($scope) {
+    $scope.model = {
+        foo: 'A value for foo',
+        bar: 'A value for bar',
+        baz: 'A value for baz'
     };
 
-    var ServiceFunction = function() {
-        this.model = localModel;
-        this.model.random = this.generateRandomString();
+    $scope.className = 'red';
+    $scope.styleObj = {
+        color:'red'
     };
 
-    ServiceFunction.prototype.generateRandomString = function() {
-        var stringArray = [];
-
-        for (var i =0; i < 10; i++) {
-            var charCode = Math.floor(Math.random()*26);
-            var char = String.fromCharCode(charCode + 97);
-            stringArray.push(char);
-        }
-
-        return stringArray.join('');
-    };
-
-    this.$get = function() {
-        return new ServiceFunction();
-    };
-
-    this.setModel = function(newModel) {
-        localModel = newModel;
-    };
-};
-
-var SimpleControllerFunction = function($scope,myservice) {
-    $scope.model = myservice.model;
-    $scope.newRandom = '';
-
+    $scope.data = ['one','two','three','four'];
     $scope.onClick = function() {
-        $scope.model.random = myservice.generateRandomString();
+        $scope.className = ($scope.className === 'red') ? 'blue' : 'red';
     };
-
-    $scope.newRandom = myservice.generateRandomString();
 };
 
-app.provider('SimpleService',SimpleServiceProvider);
-
-app.controller('SimpleControllerA',['$scope','SimpleService',SimpleControllerFunction]);
-app.controller('SimpleControllerB',['$scope','SimpleService',SimpleControllerFunction]);
-
-var AppConfig = function(SimpleProvider) {
-    var myNewModel = {
-        foo: 'A configured value for foo',
-        bar: 'A configured value for bar',
-        baz: 'A configured value for baz',
-        random: ''
+var BodyControllerFunction = function($scope) {
+    $scope.partials = {
+        ngrepeat:'/partials/ngrepeat.html',
+        ngclass:'/partials/ngclass.html',
+        ngstyle:'/partials/ngstyle.html',
+        ngoptions:'/partials/ngoptions.html'
     };
-
-    SimpleProvider.setModel(myNewModel);
+    $scope.activePartial = '';
+    $scope.onClick = function(url) {
+        $scope.activePartial=url;
+    };
 };
 
-app.config(['SimpleServiceProvider',AppConfig]);
+app.controller('SimpleController',['$scope',SimpleControllerFunction]);
+app.controller('BodyController',['$scope',BodyControllerFunction]);
+
 

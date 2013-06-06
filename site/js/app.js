c0716b0e688cfede18695f64d9ac5c88d5f2721b
bhodgen <bhodgen@armt.com>
2013-06-06 16:58:07 -0500

    demo-14

--- site/js/app.js
+++ site/js/app.js
@@ -5,6 +5,7 @@ var DirectiveFactoryFunction = function() {
     var genString = function(length) {
         var stringArray = [];
         var len = parseInt(length);
+
         var curMod = Math.floor(Math.random()*5) + 5;
 
         for (var i =0; i < len; i++) {
@@ -17,25 +18,40 @@ var DirectiveFactoryFunction = function() {
             stringArray.push(char);
         }
 
-        return stringArray.join('');
+        return stringArray;
     };
 
     var DirectiveLinkFunction = function(scope,ele,attrs) {
-        scope.randomString = genString(scope.stringLength);
+        scope.modifyDom = function() {
+            var strArr = genString(scope.stringLength);
+            var colorArray = scope.stringColors.split(',');
+
+            for (var i =0; i < strArr.length;i++) {
+                var spanEle = angular.element('<span></span>');
+                var colorIndex = Math.floor(Math.random()*colorArray.length);
+                spanEle.append(strArr[i]);
+                spanEle.css('color',colorArray[colorIndex]);
+                ele.append(spanEle);
+            }
+
+        };
 
         scope.$watch('stringLength', function(newVal,oldVal) {
             if (newVal === oldVal) {
                 return;
             }
-            scope.randomString = genString(scope.stringLength);
+            ele.text('');
+            scope.modifyDom();
         });
+
+        scope.modifyDom();
     };
 
     var DirectiveDefinitionObject = {
         restrict: 'E',
-        template: '<p>{{randomString}}</p>',
         scope: {
-            stringLength:'@'
+            stringLength:'@',
+            stringColors:'@'
         },
         link: DirectiveLinkFunction
     };
@@ -45,6 +61,7 @@ var DirectiveFactoryFunction = function() {
 
 var SimpleControllerFunction = function($scope) {
     $scope.len = 10;
+    $scope.colors = 'red,green,blue,orange';
 };
 
 app.directive('randomText',DirectiveFactoryFunction);
